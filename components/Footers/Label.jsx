d02d97139731bc776ff14750b0992b830c670a87
j-ram1 <ramsaijangala1854@gmail.com>
2025-10-28 01:38:10 +0530

    Label.jsx

--- components/Footers/Label.jsx
+++ components/Footers/Label.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Label() {
     return (
-        <p className='flex gap-x-1 items-center justify-center font-medium font-normal text-white text-sm py-1 bg-black/80 text-center'>Made with <span className='text-xl text-[#e25555]'>❤</span> by <a className='underline' target='_blank' >Ram Jangala</a></p>
+        <p>Ram Jangala</p>
         
     )
 }
