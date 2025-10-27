cbcb5160bd64847e8c36c8fe3098a34938fd0b52
j-ram1 <ramsaijangala1854@gmail.com>
2025-10-28 01:22:49 +0530

    Label.jsx

--- components/Footers/Label.jsx
+++ components/Footers/Label.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Label() {
     return (
-        <p className='flex gap-x-1 items-center justify-center font-medium font-normal text-white text-sm py-1 bg-black/80 text-center'>Made with <span className='text-xl text-[#e25555]'>❤</span> by <a className='underline' target='_blank' href='https://shreyansh-gupta.vercel.app/'>Shreyansh Gupta</a></p>
+        <p className='flex gap-x-1 items-center justify-center font-medium font-normal text-white text-sm py-1 bg-black/80 text-center'>Made with <span className='text-xl text-[#e25555]'>❤</span> by <a className='underline' target='_blank' >Ram Jangala</a></p>
         
     )
 }
